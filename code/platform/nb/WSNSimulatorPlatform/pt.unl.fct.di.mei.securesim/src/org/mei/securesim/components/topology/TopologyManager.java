b98c5d9d94a885880807f2fddf22d4db30c3f870
posilva <posilva@465017ae-f974-11de-bfc1-19433896a63b>
2010-11-17 00:17:04 +0000

    Escrita

--- code/platform/nb/WSNSimulatorPlatform/pt.unl.fct.di.mei.securesim/src/org/mei/securesim/components/topology/TopologyManager.java
+++
@@ -1,9 +0,0 @@
-package org.mei.securesim.components.topology;
-
-import java.awt.Rectangle;
-import java.util. Vector;
-import org.mei.securesim.core.nodes.Node;
-
-public abstract class TopologyManager {
-    public abstract  Vector<Node>  apply(Rectangle rect, Vector<Node> nodes) ;
-}
